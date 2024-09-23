f2d135b0fb7a14b032660b2559d43b596ffe214d
s19835 <s19835@sci.pdn.ac.lk>
2024-09-23 21:11:20 +0530

    Add: note base command

--- cmd/note.go
+++ cmd/note.go
@@ -4,8 +4,6 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
@@ -14,9 +12,6 @@ var noteCmd = &cobra.Command{
 	Use:   "note",
 	Short: "A note can be anything that you want to review",
 	Long:  `Learn anything by adding note and reviewing them regulary, make it stick in your mind.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("note called")
-	},
 }
 
 func init() {
